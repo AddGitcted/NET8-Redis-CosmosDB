a59556aa0ed46de193e20ebdf83257c9d5d46162
Addicted <schmidt.jeanbaptiste@gmail.com>
2024-11-08 16:40:36 +0100

    Split consumer/producer add kafka producer service add kafka docker compose

---
+++ ProducerAPI/Services/IKafkaProducerService.cs
@@ -0,0 +1,7 @@
+﻿namespace ProducerAPI.Services
+{
+    public interface IKafkaProducerService
+    {
+        Task ProduceAsync(string topic, string message);
+    }
+}
---
+++ ProducerAPI/Services/KafkaProducerService.cs
@@ -0,0 +1,20 @@
+﻿using Confluent.Kafka;
+
+namespace ProducerAPI.Services
+{
+    public class KafkaProducerService
+    {
+        private readonly IProducer<Null, string> _producer;
+
+        public KafkaProducerService(ProducerConfig config)
+        {
+            _producer = new ProducerBuilder<Null,  string>(config).Build();
+        }
+
+        public async Task ProduceAsync(string topic, string message)
+        {
+            await _producer.ProduceAsync(topic, new Message<Null, string> { Value = message });
+            Console.WriteLine($"Produced message: {message}");
+        }
+    }
+}
